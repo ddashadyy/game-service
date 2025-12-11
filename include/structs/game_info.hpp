22a3803aaa4b7c5cf38cd89ba789b63f2faee7cd
ddashadyy <poletaevgena75@gmail.com>
2025-12-11 18:45:59 +0800

    rm comments

--- include/structs/game_info.hpp
+++ include/structs/game_info.hpp
@@ -1,36 +1,31 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
-#include <cstdint> 
 
 namespace entities {
 
-struct GameInfo 
+struct GameInfo
 {
-    // Основная информация
     std::string id;
     std::string name;
     std::string slug;
     std::string summary;
-    
-    // Рейтинги и популярность
+
     double rating;
     std::int32_t hypes;
-    
-    // Даты
+
     std::string firstReleaseDate;
     std::vector<std::string> releaseDates;
-    
-    // Медиа
+
     std::string coverUrl;
     std::vector<std::string> artworkUrls;
     std::vector<std::string> screenshots;
-    
-    // Классификации
+
     std::vector<std::string> genres;
     std::vector<std::string> themes;
     std::vector<std::string> platforms;
 };
 
-} // namespace entities 
+} // namespace entities
